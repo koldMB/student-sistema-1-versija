92e5563c2e17c053811032ddd3d5132cc9ea9a74
Matas Bugailiškis <matbugail@gmail.com>
2026-02-05 00:27:36 +0200

    Pradėtas meniu

--- vektoriai.cpp
+++ vektoriai.cpp
@@ -5,8 +5,10 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <random>
 #include <iomanip>
 #include <cctype>
+#include <cstdlib>
 
 
 using std::cout;
@@ -15,8 +17,8 @@ using std::vector;
 using std::string;
 using std::setw;
 using std::sort;
-using std::isdigit;
-using std::stoi;
+using std::isdigit; //tikrina ar char yra skaitmuo
+using std::stoi; //string to int
 using std::left;
 
 struct studentas {
@@ -33,12 +35,90 @@ void skaičiavimai(vector<studentas> &stud);
 void išvestis(const vector<studentas> &stud, int &MaxPav, int &MaxVard);
 void raidės(int &MaxPav, int &MaxVard, vector <studentas> &stud);
 bool isInteger(const string& s);
+void skaitymas(vector<studentas> &stud, bool genPaz, int n, int nd_sk, bool genVardPav, int VardPavSk);
+
 
 int main() {
     studentas temp;
     int MaxPav = 0, MaxVard = 0;
     vector<studentas> stud;
-    skaitymas(stud);
+    int pasirinkimas;
+    do{
+        cout << "1. Įvesti duomenis ranka\n";
+        cout << "2. Generuoti pažymius\n";
+        cout << "3. Generuoti vardus ir pavardes bei pažymius\n";
+        cout << "4. Darbo pabaiga\n";
+        cout << "Pasirinkite veiksmą: "; cin >> pasirinkimas;
+        if(pasirinkimas < 1 || pasirinkimas > 4){
+            cout << "Klaidinga įvestis. Bandykite dar kartą.\n";
+            continue;
+        }
+        switch (pasirinkimas) 
+        {
+            case 1:
+                skaitymas(stud);
+            break;
+            case 2:
+                {
+                    int n, nd_sk;
+                    cout << "Įveskite kiek studentų duomenų norite generuoti: ";
+                    string n_str;
+                    cin >> n_str;
+                    while(!isInteger(n_str) || stoi(n_str) <= 0) {
+                        cout << "Klaidinga įvestis. Bandykite dar kartą: ";
+                        cin >> n_str;
+                    }
+                    n = stoi(n_str);
+                    cout << "Įveskite kiek namų darbų pažymių norite generuoti: ";
+                    string nd_sk_str;
+                    cin >> nd_sk_str;
+                    while(!isInteger(nd_sk_str) || stoi(nd_sk_str) <= 0) {
+                        cout << "Klaidinga įvestis. Bandykite dar kartą: ";
+                        cin >> nd_sk_str;
+                    }
+                    nd_sk = stoi(nd_sk_str);
+                    skaitymas(stud, true, n, nd_sk, false, 0);
+                }
+            break;
+            case 3:
+                {
+                    int n, nd_sk, VardPavSk;
+                    cout << "Įveskite kiek studentų duomenų norite generuoti: ";
+                    string n_str;
+                    cin >> n_str;
+                    while(!isInteger(n_str) || stoi(n_str) <= 0) {
+                        cout << "Klaidinga įvestis. Bandykite dar kartą: ";
+                        cin >> n_str;
+                    }
+                    n = stoi(n_str);
+                    cout << "Įveskite kiek namų darbų pažymių norite generuoti: ";
+                    string nd_sk_str;
+                    cin >> nd_sk_str;
+                    while(!isInteger(nd_sk_str) || stoi(nd_sk_str) <= 0) {
+                        cout << "Klaidinga įvestis. Bandykite dar kartą: ";
+                        cin >> nd_sk_str;
+                    }
+                    nd_sk = stoi(nd_sk_str);
+                    cout << "Įveskite vardų ir pavardžių sąrašo dydį: ";
+                    string VardPavSk_str;
+                    cin >> VardPavSk_str;
+                    while(!isInteger(VardPavSk_str) || stoi(VardPavSk_str) <= 0) {
+                        cout << "Klaidinga įvestis. Bandykite dar kartą: ";
+                        cin >> VardPavSk_str;
+                    }
+                    VardPavSk = stoi(VardPavSk_str);
+                    skaitymas(stud, true, n, nd_sk, true, VardPavSk);
+                }
+            break;
+            case 4:
+                {
+                cout << "Programa baigia darbą.\n";
+                exit(0);
+                }
+            break;
+        }
+    }while(true);
+    skaitymas(stud, false, 0, 0, false, 0);
     skaičiavimai(stud);
     raidės(MaxPav, MaxVard, stud);
     išvestis(stud, MaxPav, MaxVard);
@@ -72,6 +152,7 @@ void skaitymas(vector<studentas> &stud)
         }
         nd_sk = stoi(nd_sk_str);
         temp.nd.resize(nd_sk);
+
         for(int j = 0; j < nd_sk; j++) {
             cout << "Įveskite " << j + 1 << " namų darbų įvertinimą: ";
             string nd_str;
@@ -147,7 +228,7 @@ bool isInteger(const string& s)
 {
     if (s.empty()) return false;
 
-    int start = (s[0] == '-' || s[0] == '+') ? 1 : 0;
+    int start = (s[0] == '-' || s[0] == '+') ? 1 : 0; //praleidzia +/- ženklus
 
     for (int i = start; i < s.size(); i++)
         if (!isdigit(s[i]))
