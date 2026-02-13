f0ef9e33a467c219b1bdc0218d52543c85381a60
Matas Bugailiškis <matbugail@gmail.com>
2026-02-13 11:11:16 +0200

    Pataisyta masyvų versija kad būtų tas pats funkcionalumas kaip ir vektoriai

--- masyvai.cpp
+++ masyvai.cpp
@@ -6,6 +6,9 @@
 #include <algorithm>
 #include <iomanip>
 #include <cctype>
+#include <cstdlib>
+#include <random>
+#include <chrono>
 
 using std::cout;
 using std::cin;
@@ -21,9 +24,9 @@ struct studentas {
     string pavarde;
     int *nd = nullptr;
     int nd_sk = 0;
-    int egz;
-    double vidurkis;
-    double mediana;
+    int egz = 0;
+    double vidurkis = 0;
+    double mediana = 0;
 };
 
 void skaitymas(studentas* &stud, int &n);
@@ -31,17 +34,124 @@ void skaičiavimai(studentas* stud, int n);
 void išvestis(const studentas* stud, int n, int MaxPav, int MaxVard);
 void raidės(int &MaxPav, int &MaxVard, studentas* stud, int n);
 bool isInteger(const string& s);
+void generuotiPazymius(studentas* &stud, int n, int nd_sk);
+void ivestiVarPav(studentas* &stud, int n);
+void skaitymasVardaiPavardes(studentas* &stud, int n);
 
 int main() {
     studentas* stud = nullptr;
     int n = 0;
     int MaxPav = 0, MaxVard = 0;
+    int pasirinkimas;
+    do{
+        cout << "1. Įvesti duomenis ranka\n"; 
+        cout << "2. Generuoti pažymius\n";
+        cout << "3. Generuoti vardus ir pavardes bei pažymius\n";
+        cout << "4. Darbo pabaiga\n";
+        cout << "Pasirinkite veiksmą: ";
+        string pasirinkimas_str;
+        cin >> pasirinkimas_str;
+        while(!isInteger(pasirinkimas_str) || stoi(pasirinkimas_str) < 1 || stoi(pasirinkimas_str) > 4) {
+            cout << "Klaidinga įvestis. Bandykite dar kartą: ";
+            cin >> pasirinkimas_str;
+        }
+        pasirinkimas = stoi(pasirinkimas_str);
 
-    skaitymas(stud, n);
-    skaičiavimai(stud, n);
-    raidės(MaxPav, MaxVard, stud, n);
-    išvestis(stud, n, MaxPav, MaxVard);
-
+        switch(pasirinkimas) {
+            case 1: 
+                {
+                    skaitymas(stud, n);
+                    skaičiavimai(stud, n);
+                    raidės(MaxPav, MaxVard, stud, n);
+                    išvestis(stud, n, MaxPav, MaxVard);
+                    for (int i = 0; i < n; ++i) {
+                        delete[] stud[i].nd;
+                    }
+                    delete[] stud;
+                    stud = nullptr;
+                }
+            break;
+            case 2:
+                {
+                    int n, nd_sk;
+                    cout << "Įveskite kiek studentų duomenų norite generuoti: ";
+                    string n_str;
+                    cin >> n_str;
+                    while(!isInteger(n_str) || stoi(n_str) <= 0) {
+                        cout << "Klaidinga įvestis. Bandykite dar kartą: ";
+                        cin >> n_str;
+                    }
+                    n = stoi(n_str);
+                    cout << "Įveskite kiek namų darbų pažymių norite generuoti: ";
+                    string nd_sk_str;
+                    cin >> nd_sk_str;
+                    while(!isInteger(nd_sk_str) || stoi(nd_sk_str) <= 0) {
+                        cout << "Klaidinga įvestis. Bandykite dar kartą: ";
+                        cin >> nd_sk_str;
+                    }
+                    nd_sk = stoi(nd_sk_str);
+                    stud = new studentas[n];
+                    for(int i = 0; i < n; i++) {
+                        stud[i].nd_sk = nd_sk;
+                        stud[i].nd = new int[nd_sk];
+                    }
+                    skaitymasVardaiPavardes(stud, n); // paprastas vedimas vardų ir pavardžių
+                    generuotiPazymius(stud, n, nd_sk); // generuoja pažymius
+                    skaičiavimai(stud, n);
+                    raidės(MaxPav, MaxVard, stud, n);
+                    išvestis(stud, n, MaxPav, MaxVard);
+                    for (int i = 0; i < n; ++i) {
+                        delete[] stud[i].nd;
+                    }
+                    delete[] stud;
+                    stud = nullptr;
+                }
+            break;
+            case 3:
+                {
+                    int n, nd_sk;
+                    cout << "Įveskite kiek studentų duomenų norite generuoti: ";
+                    string n_str;
+                    cin >> n_str;
+                    while(!isInteger(n_str) || stoi(n_str) <= 0) {
+                        cout << "Klaidinga įvestis. Bandykite dar kartą: ";
+                        cin >> n_str;
+                    }
+                    n = stoi(n_str);
+                    cout << "Įveskite kiek namų darbų pažymių norite generuoti: ";
+                    string nd_sk_str;
+                    cin >> nd_sk_str;
+                    while(!isInteger(nd_sk_str) || stoi(nd_sk_str) <= 0) {
+                        cout << "Klaidinga įvestis. Bandykite dar kartą: ";
+                        cin >> nd_sk_str;
+                    }
+                    nd_sk = stoi(nd_sk_str);
+                    
+                    stud = new studentas[n];
+                    ivestiVarPav(stud, n); // generuoja vardus ir pavardes
+                    for(int i = 0; i < n; i++) {
+                        stud[i].nd_sk = nd_sk;
+                        stud[i].nd = new int[nd_sk];
+                    }
+                    generuotiPazymius(stud, n, nd_sk); // generuoja pažymius
+                    skaičiavimai(stud, n);
+                    raidės(MaxPav, MaxVard, stud, n);
+                    išvestis(stud, n, MaxPav, MaxVard);
+                    
+                    for (int i = 0; i < n; ++i) {
+                        delete[] stud[i].nd;
+                    }
+                    delete[] stud;
+                    stud = nullptr;
+                }
+            break;
+            case 4:
+                {
+                    exit(0);
+                }
+            break;
+        }
+    } while(true);
     for (int i = 0; i < n; ++i) {
         delete[] stud[i].nd;
     }
@@ -172,3 +282,34 @@ bool isInteger(const string& s)
     return start < s.size();
 }
 
+void generuotiPazymius(studentas* &stud, int n, int nd_sk)
+{
+    std::random_device rd;
+    auto seed = std::seed_seq{rd(), static_cast<unsigned int>(std::chrono::high_resolution_clock::now().time_since_epoch().count())};
+    std::mt19937 gen(seed);
+    std::uniform_int_distribution<> dis(1, 10);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < nd_sk; j++) {
+            stud[i].nd[j] = dis(gen);
+        }
+        stud[i].egz = dis(gen);
+    }
+}
+void ivestiVarPav(studentas* &stud, int n)
+{
+    for(int i = 0; i < n; i++) {
+        stud[i].vardas = "Vardas" + std::to_string(i + 1);
+        stud[i].pavarde = "Pavarde" + std::to_string(i + 1);
+    }
+}
+
+void skaitymasVardaiPavardes(studentas* &stud, int n)
+{
+    for(int i = 0; i < n; i++) {
+        cout << "Įveskite " << i + 1 << " studento vardą: ";
+        cin >> stud[i].vardas;
+        cout << "Įveskite " << i + 1 << " studento pavardę: ";
+        cin >> stud[i].pavarde;
+    }
+}
