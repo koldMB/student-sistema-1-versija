09f08bac1eb0084409a05e6f22e3510b48e189a3
Matas Bugailiškis <matbugail@gmail.com>
2026-03-18 14:33:32 +0200

    template overloads naujoms versijoms

--- Common.h
+++ Common.h
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <vector>
+#include <list>
+#include <deque>
 
 using std::string;
 using std::vector;
@@ -22,5 +24,23 @@ void atrinkimasAutomatiskas(const vector<studentas> &stud, vector<studentas> &ge
 void Bandymas1_FailuGeneravimas(int sizes[5]);
 void Bandymas2_DuomenuApdorojimas(int sizes[5], int pas);
 
+// Templatai studentui
+template<typename T1, typename T2, typename T3>
+void atrinkimas(const T1 &stud, T2 &atrinkti, T3 &neatrinkti) {
+    vector<studentas> temp_stud(stud.begin(), stud.end());
+    vector<studentas> temp_atrinkti, temp_neatrinkti;
+    atrinkimas(temp_stud, temp_atrinkti, temp_neatrinkti);
+    atrinkti.assign(temp_atrinkti.begin(), temp_atrinkti.end());
+    neatrinkti.assign(temp_neatrinkti.begin(), temp_neatrinkti.end());
+}
+
+template<typename T1, typename T2, typename T3>
+void atrinkimasAutomatiskas(const T1 &stud, T2 &geri, T3 &blogi) {
+    vector<studentas> temp_stud(stud.begin(), stud.end());
+    vector<studentas> temp_geri, temp_blogi;
+    atrinkimasAutomatiskas(temp_stud, temp_geri, temp_blogi);
+    geri.assign(temp_geri.begin(), temp_geri.end());
+    blogi.assign(temp_blogi.begin(), temp_blogi.end());
+}
 
 #endif // COMMON_H
