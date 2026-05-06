4fb4dd426502b26c453aa2bd3afd79371385e212
Matas Bugailiškis <matbugail@gmail.com>
2026-05-06 16:03:15 +0300

    google testai
    Co-authored-by: koldMB <140719921+koldMB@users.noreply.github.com>

--- rule_of_5_tests.cpp
+++ rule_of_5_tests.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 #include "Common.h"
 #include <windows.h>
-#include <assert.h>
+#include <gtest/gtest.h>
 #include <exception>
 #include <sstream>
 
 // Test 1: Numatytasis Konstruktorius
-void test_numatytasis_konstruktorius() {
-    std::cout << "Test 1: Numatytasis Konstruktorius... ";
+TEST(StudentasTest, NumatytasisKonstruktorius) {
     Studentas s;
-    assert(s.vardas() == "");
-    assert(s.egzaminas() == 0);
-    assert(s.nd().empty());
-    std::cout << "veikia\n";
+    EXPECT_EQ(s.vardas(), "");
+    EXPECT_EQ(s.egzaminas(), 0);
+    EXPECT_TRUE(s.nd().empty());
 }
 
 // Test 2: Kopijavimo Konstruktorius
-void test_kopijavimo_konstruktorius() {
-    std::cout << "Test 2: Kopijavimo Konstruktorius... ";
+TEST(StudentasTest, KopijantimoKonstruktorius) {
     Studentas original;
     original.setVardas("test1");
     original.setPavarde("test2");
@@ -25,16 +22,14 @@ void test_kopijavimo_konstruktorius() {
     original.addNd(7.0);
     
     Studentas copy(original);
-    assert(copy.vardas() == "test1");
-    assert(copy.pavarde() == "test2");
-    assert(copy.egzaminas() == 8.5);
-    assert(copy.nd()[0] == 7.0);
-    std::cout << "veikia\n";
+    EXPECT_EQ(copy.vardas(), "test1");
+    EXPECT_EQ(copy.pavarde(), "test2");
+    EXPECT_EQ(copy.egzaminas(), 8.5);
+    EXPECT_EQ(copy.nd()[0], 7.0);
 }
 
 // Test 3: Kopijavimo Priskyrimo Operatorius
-void test_kopijavimo_priskyrimas() {
-    std::cout << "Test 3: Kopijavimo Priskyrimo Operatorius... ";
+TEST(StudentasTest, KopijantimoPeriskyrimo) {
     Studentas s1;
     s1.setVardas("test3");
     s1.setPavarde("test4");
@@ -42,15 +37,13 @@ void test_kopijavimo_priskyrimas() {
     
     Studentas s2;
     s2 = s1;
-    assert(s2.vardas() == "test3");
-    assert(s2.pavarde() == "test4");
-    assert(s2.nd()[0] == 6.5);
-    std::cout << "veikia\n";
+    EXPECT_EQ(s2.vardas(), "test3");
+    EXPECT_EQ(s2.pavarde(), "test4");
+    EXPECT_EQ(s2.nd()[0], 6.5);
 }
 
 // Test 4: Perkėlimo Konstruktorius
-void test_perkėlimo_konstruktorius() {
-    std::cout << "Test 4: Perkėlimo Konstruktorius... ";
+TEST(StudentasTest, PerkėlimoKonstruktorius) {
     Studentas original;
     original.setVardas("test5");
     original.setPavarde("test6");
@@ -72,31 +65,28 @@ void test_perkėlimo_konstruktorius() {
     Studentas moved(std::move(original));
     
     // Check moved object has all original data
-    assert(moved.vardas() == orig_vardas);
-    assert(moved.pavarde() == orig_pavarde);
-    assert(moved.egzaminas() == orig_egz);
-    assert(moved.nd().size() == orig_nd_size);
-    assert(moved.mediana() == orig_mediana);
-    assert(moved.vidurkis() == orig_vidurkis);
-    assert(moved.nd()[0] == 8.0);
+    EXPECT_EQ(moved.vardas(), orig_vardas);
+    EXPECT_EQ(moved.pavarde(), orig_pavarde);
+    EXPECT_EQ(moved.egzaminas(), orig_egz);
+    EXPECT_EQ(moved.nd().size(), orig_nd_size);
+    EXPECT_EQ(moved.mediana(), orig_mediana);
+    EXPECT_EQ(moved.vidurkis(), orig_vidurkis);
+    EXPECT_EQ(moved.nd()[0], 8.0);
     
     // Check original object is empty/cleared
-    assert(original.vardas() == "");
-    assert(original.pavarde() == "");
-    assert(original.nd().empty());
+    EXPECT_EQ(original.vardas(), "");
+    EXPECT_EQ(original.pavarde(), "");
+    EXPECT_TRUE(original.nd().empty());
     
     // Add new data to moved object and verify recalculation
     moved.addNd(9.0);
-    assert(moved.nd().size() == orig_nd_size + 1);
-    assert(moved.nd().back() == 9.0);
-    
-    std::cout << "veikia\n";
+    EXPECT_EQ(moved.nd().size(), orig_nd_size + 1);
+    EXPECT_EQ(moved.nd().back(), 9.0);
 }
 
 
 // Test 5: Perkėlimo Priskyrimo Operatorius
-void test_perkėlimo_priskyrimas() {
-    std::cout << "Test 5: Perkėlimo Priskyrimo Operatorius... ";
+TEST(StudentasTest, PerkelimoPerskyrimas) {
     Studentas s1;
     s1.setVardas("test7");
     s1.setPavarde("test8");
@@ -122,30 +112,27 @@ void test_perkėlimo_priskyrimas() {
     s2 = std::move(s1);
     
     // Check s2 has all original s1 data
-    assert(s2.vardas() == s1_vardas);
-    assert(s2.pavarde() == s1_pavarde);
-    assert(s2.egzaminas() == s1_egz);
-    assert(s2.nd().size() == s1_nd_size);
-    assert(s2.mediana() == s1_mediana);
-    assert(s2.vidurkis() == s1_vidurkis);
-    assert(s2.nd()[0] == 9.0);
+    EXPECT_EQ(s2.vardas(), s1_vardas);
+    EXPECT_EQ(s2.pavarde(), s1_pavarde);
+    EXPECT_EQ(s2.egzaminas(), s1_egz);
+    EXPECT_EQ(s2.nd().size(), s1_nd_size);
+    EXPECT_EQ(s2.mediana(), s1_mediana);
+    EXPECT_EQ(s2.vidurkis(), s1_vidurkis);
+    EXPECT_EQ(s2.nd()[0], 9.0);
     
     // Check s1 is empty/cleared
-    assert(s1.vardas() == "");
-    assert(s1.pavarde() == "");
-    assert(s1.nd().empty());
+    EXPECT_EQ(s1.vardas(), "");
+    EXPECT_EQ(s1.pavarde(), "");
+    EXPECT_TRUE(s1.nd().empty());
     
     // Add new data to s2 and verify
     s2.addNd(7.5);
-    assert(s2.nd().size() == s1_nd_size + 1);
-    assert(s2.nd().back() == 7.5);
-    
-    std::cout << "veikia\n";
+    EXPECT_EQ(s2.nd().size(), s1_nd_size + 1);
+    EXPECT_EQ(s2.nd().back(), 7.5);
 }
 
 // Test 6: Destruktorius
-void test_destruktorius() {
-    std::cout << "Test 6: Destruktorius... ";
+TEST(StudentasTest, Destruktorius) {
     {
         Studentas s;
         s.setVardas("test9");
@@ -153,75 +140,64 @@ void test_destruktorius() {
         s.addNd(8.0);
         // Destruktorius automatiškai iškviečiamas
     }
-    std::cout << "veikia\n";
 }
 
 // Test 7: Setteriai ir Getteriai
-void test_setteriai_getteriai() {
-    std::cout << "Test 7: Setteriai ir Getteriai... ";
+TEST(StudentasTest, SetteriaIGetteriai) {
     Studentas s;
     
     s.setVardas("test10");
-    assert(s.vardas() == "test10");
+    EXPECT_EQ(s.vardas(), "test10");
     
     s.setPavarde("test11");
-    assert(s.pavarde() == "test11");
+    EXPECT_EQ(s.pavarde(), "test11");
     
     s.setEgzaminas(7.5);
-    assert(s.egzaminas() == 7.5);
+    EXPECT_EQ(s.egzaminas(), 7.5);
     
     s.setMediana(7.0);
-    assert(s.mediana() == 7.0);
+    EXPECT_EQ(s.mediana(), 7.0);
     
     s.setVidurkis(7.25);
-    assert(s.vidurkis() == 7.25);
-    
-    std::cout << "veikia\n";
+    EXPECT_EQ(s.vidurkis(), 7.25);
 }
 
 // Test 8: ND Operacijos (addNd, clearNd, resizeNd, setNdAt)
-void test_nd_operacijos() {
-    std::cout << "Test 8: ND Operacijos (addNd, clearNd, resizeNd, setNdAt)... ";
+TEST(StudentasTest, NDOperacijos) {
     Studentas s;
     
     s.addNd(5.0);
     s.addNd(6.0);
     s.addNd(7.0);
-    assert(s.nd().size() == 3);
-    assert(s.nd()[0] == 5.0);
+    EXPECT_EQ(s.nd().size(), 3);
+    EXPECT_EQ(s.nd()[0], 5.0);
     
     s.setNdAt(1, 8.5);
-    assert(s.nd()[1] == 8.5);
+    EXPECT_EQ(s.nd()[1], 8.5);
     
     s.resizeNd(5);
-    assert(s.nd().size() == 5);
+    EXPECT_EQ(s.nd().size(), 5);
     
     s.clearNd();
-    assert(s.nd().empty());
-    
-    std::cout << "veikia\n";
+    EXPECT_TRUE(s.nd().empty());
 }
 
 // Test 9: setNd ir ndRef
-void test_setnd_ndref() {
-    std::cout << "Test 9: setNd ir ndRef... ";
+TEST(StudentasTest, SetndNdref) {
     Studentas s;
     
     std::vector<double> paz = {6.0, 7.0, 8.0, 9.0};
     s.setNd(paz);
-    assert(s.nd().size() == 4);
-    assert(s.nd()[3] == 9.0);
+    EXPECT_EQ(s.nd().size(), 4);
+    EXPECT_EQ(s.nd()[3], 9.0);
     
     s.ndRef().push_back(10.0);
-    assert(s.nd().size() == 5);
-    assert(s.nd()[4] == 10.0);
-    
-    std::cout << "veikia\n";
+    EXPECT_EQ(s.nd().size(), 5);
+    EXPECT_EQ(s.nd()[4], 10.0);
 }
 
 
-int main() {
-
+int main(int argc, char** argv) {
     try{
         SetConsoleCP(CP_UTF8);
         SetConsoleOutputCP(CP_UTF8);
@@ -230,18 +206,7 @@ int main() {
         std::cerr << "Klaida keičiant console į UTF-8: " << e.what() << std::endl;
         return -2;
     }
-    std::cout << "\nPenkių taisyklės ir metodų testai\n\n";
     
-    test_numatytasis_konstruktorius();
-    test_kopijavimo_konstruktorius();
-    test_kopijavimo_priskyrimas();
-    test_perkėlimo_konstruktorius();
-    test_perkėlimo_priskyrimas();
-    test_destruktorius();
-    test_setteriai_getteriai();
-    test_nd_operacijos();
-    test_setnd_ndref();
-    
-    std::cout << "\nVisi testai praeiti\n\n";
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
