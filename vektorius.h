64ea56b20465b405dc7c52fe9ce7b1ba67786dbd
Matas Bugailiškis <matbugail@gmail.com>
2026-05-19 14:01:37 +0300

    Konstruktoriai

--- vektorius.h
+++ vektorius.h
@@ -27,6 +27,20 @@ private:
         duomenys_ = new_data;
         talpa_ = nauja_talpa;
     }
+
+public:
+    // Numatytasis konstruktorius
+    mano_vektorius() : duomenys_(nullptr), dydis_(0), talpa_(0) {}
+
+    // Konstruktorius su pradine talpa
+    explicit mano_vektorius(size_t initial_capacity)
+        : dydis_(0), talpa_(initial_capacity) {
+        if (initial_capacity > 0) {
+            duomenys_ = new T[initial_capacity];
+        } else {
+            duomenys_ = nullptr;
+        }
+    }
 };
 
 #endif // VEKTORIUS_H
